66022d42e2a1a4b3ff2d18b9d510b404c8de8841
larsi <larsi>
2004-01-31 22:33:42 +0000

    *** empty log message ***

---
+++ lisp/hash.lisp
@@ -0,0 +1,15 @@
+;;; -*- Mode: lisp; Syntax: ansi-common-lisp; Package: RETICULE -*-
+
+(defpackage reticule
+  (:use :cl)
+  (:export generate-password))
+
+(in-package :reticule)
+
+(eval-when (:load-toplevel :compile-toplevel :execute)
+  (require :util "util")
+  (require :client "client")
+  (require :server "server")
+  )
+
+(provide :reticule)
