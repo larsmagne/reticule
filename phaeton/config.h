1bf33f2e1bf328399d938d713f8e00e7924dca6c
larsi <larsi>
2004-02-22 11:32:25 +0000

    Continue/.

--- phaeton/config.h
+++ phaeton/config.h
@@ -1,5 +1,16 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
+/* Default port number Phaeton listens for clients on. */
+#define SERVER_PORT 8040
+
+/* The Reticule files. */
+#define RETICULE_HOME "/home/larsi/reticule/data/"
+
+/* The spool. */
+#define SPOOL_DIRECTORY "/mirror/var/spool/news/articles/"
+
+#define USER "nobody"
+#define GROUP "nobody"
 
 #endif
