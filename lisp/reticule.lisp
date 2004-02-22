2d4f86290675f0986e8d6e29c785205c45f6bcd0
larsi <larsi>
2004-02-22 17:11:11 +0000

    clocc

--- lisp/reticule.lisp
+++ lisp/reticule.lisp
@@ -6,11 +6,18 @@
 
 (in-package :reticule)
 
+(setq *clocc-root* "/usr/local/lib/lisp/clocc/")
+(load (concatenate 'string *clocc-root* "clocc"))
+(load (translate-logical-pathname "clocc:src;cllib;base"))
+(load (translate-logical-pathname "clocc:src;cllib;date"))
+
+(defvar *reticule-home* "/home/larsi/reticule/data/")
+
 (eval-when (:load-toplevel :compile-toplevel :execute)
   (require :util "util")
+  (require :hash "hash")
   (require :client "client")
   (require :server "server")
-  (require :hash "hash")
   )
 
 (provide :reticule)
