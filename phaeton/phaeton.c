9afd372691bedd3df815766e9adff30107128720
larsi <larsi>
2004-03-05 21:36:37 +0000

    Refactor.

--- phaeton/phaeton.c
+++ phaeton/phaeton.c
@@ -17,16 +17,13 @@
 
 #include "config.h"
 #include "phaeton.h"
+#include "misc.h"
 #include <util.h>
 
-char *reticule_home = RETICULE_HOME;
-char *spool_directory = SPOOL_DIRECTORY;
 int server_port = SERVER_PORT;
 char *version = VERSION;
 char *host_name = HOST_NAME;
 
-static GHashTable *group_table = NULL;
-static char *reverse_group_table[MAX_GROUPS];
 int active_file = 0;
 char *selected_group = NULL;
 unsigned char *group_map = NULL;
@@ -41,74 +38,11 @@ void message(FILE *client, int code, char *message) {
     fprintf(client, "%d %s\r\n", code, message);
 }
 
-char *reticule_file_name(char *name) {
-  static char file_name[1024];
-  strcpy(file_name, reticule_home);
-  strcat(file_name, "/");
-  strcat(file_name, name);
-  return file_name;
-}
-
-void read_groups_file() {
-  FILE *fp;
-  char group[MAX_GROUP_NAME_LENGTH], *g;
-  int group_id;
-  
-  group_table = g_hash_table_new(g_str_hash, g_str_equal);
-
-  if ((fp = fopen(reticule_file_name("groups.dat"), "r")) == NULL) {
-    fprintf(stderr, "Couldn't find groups.dat file\n");
-    return;
-  }
-
-  while (fscanf(fp, "%s %d\n", group, &group_id) != EOF) {
-    g = cmalloc(strlen(group)+1);
-    strcpy(g, group);
-
-    g_hash_table_insert(group_table, (gpointer)g, (gpointer)group_id);
-    reverse_group_table[group_id] = g;
-    if (group_id > max_group_id)
-      max_group_id = group_id;
-  }
-  fclose(fp);
-}
-
-char *group_id_to_name(int id) {
-  return reverse_group_table[id];
-}
-
-int group_name_to_id(char *group) {
-  return (int)g_hash_table_lookup(group_table, (gpointer)group);
-}
-
 void open_active_file() {
   if ((active_file = open(reticule_file_name("active"), O_RDONLY)) == 0) {
     fprintf(stderr, "Couldn't find active file\n");
     return;
   }
-}
-
-char *get_overview_name(char *group, char *extension) {
-  static char file_name[1024];
-  char *f, *g = group;
-
-  strcpy(file_name, reticule_home);
-  strcat(file_name, "/overview/");
-  f = file_name + strlen(file_name);
-  while (*g) {
-    *f++ = *g;
-    *f++ = '/';
-    if (strchr(g, '.')) 
-      g = strchr(g, '.') + 1;
-    else 
-      break;
-  } 
-  *f = 0;
-
-  strcat(file_name, group);
-  strcat(file_name, extension);
-  return file_name;
-  
 }
 
 int select_group(char *group) {
@@ -144,30 +78,6 @@ int select_group(char *group) {
   client.selected_group = 0;
 
   return 1;
-}
-
-int isnumerical(char *string) {
-  while (*string) {
-    if (! isdigit(*string))
-      return 0;
-    string++;
-  }
-  return 1;
-}
-
-char *get_article_name(char *group, int article) {
-  static char file_name[1024];
-  char *f = file_name + strlen(spool_directory);
-  
-  snprintf(file_name, 1024, "%s/%s/%d", spool_directory, group, article);
-  
-  while (*f != 0) {
-    if (*f == '.')
-      *f = '/';
-    f++;
-  }
-
-  return file_name;
 }
 
 int message_present(int article) {
--- phaeton/phaeton.h
+++ phaeton/phaeton.h
@@ -5,8 +5,6 @@
 #define MAX_GROUP_NAME_LENGTH 1024
 #define MAX_GROUPS 16000
 
-extern char *reticule_home;
-extern char *spool_directory;
 extern int server_port;
 extern int server_socket;
 extern char *version;
@@ -18,15 +16,9 @@ extern char *client_address;
 
 void start_server(int port);
 void message(FILE *client, int code, char *message);
-char *group_id_to_name(int id);
-int group_name_to_id(char *group);
-void read_groups_file();
 void open_active_file();
 int select_group(char *group);
-int isnumerical(char *string);
-char *get_article_name(char *group, int article);
 int message_present(int article);
-char *get_overview_name(char *group, char *extension);
 int selectedp();
 void tputs(char *string, FILE *client);
 
